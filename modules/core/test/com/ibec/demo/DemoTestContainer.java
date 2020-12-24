7113c307be8abf6fad800079a5f0a9a75b139a4e
Vladimir Savinykh <ilovethisjam@gmail.com>
2020-12-24 18:17:35 +0600

    React Front initialized.

--- modules/core/test/com/ibec/demo/DemoTestContainer.java
+++ modules/core/test/com/ibec/demo/DemoTestContainer.java
@@ -11,10 +11,9 @@ public class DemoTestContainer extends TestContainer {
     public DemoTestContainer() {
         super();
         //noinspection ArraysAsListWithZeroOrOneArgument
-        appComponents = new ArrayList<>(Arrays.asList(
-                // list add-ons here: "com.haulmont.reports", "com.haulmont.addon.bproc", etc.
-                "com.haulmont.cuba"
-        ));
+        appComponents = Arrays.asList(
+                "com.haulmont.cuba",
+                "com.haulmont.addon.restapi");
         appPropertiesFiles = Arrays.asList(
                 // List the files defined in your web.xml
                 // in appPropertiesConfig context parameter of the core module
